a50a44add3aca4c80155115cd38e0d80a9d6639c
Devraj Singh <aol.devraj@aol.com>
2025-02-22 10:09:05 +0530

    modified:   static/main.js 	modified:   templates/about.html 	modified:   templates/blogs.html 	modified:   templates/contact.html 	modified:   templates/homepage.html 	modified:   templates/privacy_policy.html

--- static/main.js
+++ static/main.js
@@ -1,33 +1,4 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // Navbar HTML
-    const navbarHtml = `
-    <nav class="navbar navbar-expand-lg">
-        <div class="container-fluid">
-            <a class="navbar-brand text-light" href="#">Core</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse fw-bold" id="navbarNav">
-                <ul class="navbar-nav ms-auto">
-                    <li class="nav-item active">
-                        <a class="nav-link text-dark" href="/">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link text-light" href="/about">About</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link text-light" href="/contact">Contact Us</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link text-light" href="/blogs">Blogs</a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </nav>
-    `;
-
-    // Footer HTML
     const footerHtml = `
     <footer class="text-light py-4 mt-5">
         <div class="container">
@@ -70,6 +41,5 @@ document.addEventListener('DOMContentLoaded', function() {
         </div>
     </footer>
     `;
-    document.body.insertAdjacentHTML('afterbegin', navbarHtml);
     document.body.insertAdjacentHTML('beforeend', footerHtml);
 });
